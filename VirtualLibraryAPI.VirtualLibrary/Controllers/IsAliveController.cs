62502eb1bca123244de09c4c84fd1e400cc750ba
Oleksii Lesko <Oleksii.Lesko@nice.com>
2023-03-16 17:06:53 +0200

    Create a IsAlive controller with logger

---
+++ VirtualLibraryAPI.VirtualLibrary/Controllers/IsAliveController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualLibraryAPI.Library.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class IsAliveController 
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        private readonly ILogger<IsAliveController> _logger;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="logger"></param>
+        public IsAliveController(ILogger<IsAliveController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
