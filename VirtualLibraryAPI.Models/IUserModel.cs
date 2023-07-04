cdcd207561ffe6d4d4f84b3b5b950706ad0d1f96
Oleksii Lesko <Oleksii.Lesko@nice.com>
2023-07-04 13:31:30 +0300

    Added interface for user model

---
+++ VirtualLibraryAPI.Models/IUserModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualLibraryAPI.Models
+{
+    /// <summary>
+    /// Interface for user
+    /// </summary>
+    public interface IUserModel
+    {
+        /// <summary>
+        /// Method for get all users
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<Domain.DTOs.User> GetAllUsers();
+        /// <summary>
+        /// Method for get user by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Domain.DTOs.User GetUserById(int id);
+        /// <summary>
+        /// Method for add user
+        /// </summary>
+        /// <param name="book"></param>
+        /// <returns></returns>
+        Domain.DTOs.User AddUser(Domain.DTOs.User user);
+        /// <summary>
+        /// Method for delete user
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Domain.DTOs.User DeleteUser(int id);
+        /// <summary>
+        /// Get all users for response DTO
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<Domain.DTOs.User> GetAllUsersResponse();
+        /// <summary>
+        /// Get  user by id in model
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Domain.DTOs.User GetUserByIdResponse(int id);
+        /// <summary>
+        /// Method for update user data
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="article"></param>
+        /// <returns></returns>
+        Domain.DTOs.User UpdateUser(int id, Domain.DTOs.User user);
+    }
+}
