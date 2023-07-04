78e334a9a6e52b9056a58d73f10e5ff170aed5a8
Oleksii Lesko <Oleksii.Lesko@nice.com>
2023-07-04 13:35:39 +0300

    Added method update user data

--- VirtualLibraryAPI.Repository/IUserRepository.cs
+++ VirtualLibraryAPI.Repository/IUserRepository.cs
@@ -6,7 +6,51 @@ using System.Threading.Tasks;
 
 namespace VirtualLibraryAPI.Repository
 {
-    internal class IUserRepository
+    /// <summary>
+    /// Article repository interface 
+    /// </summary>
+    public interface IUserRepository
     {
+        /// <summary>
+        /// Method for get all users
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<Domain.DTOs.User> GetAllUsers();
+        /// <summary>
+        /// Method for get user by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Domain.DTOs.User GetUserById(int id);
+        /// <summary>
+        /// Method for add user
+        /// </summary>
+        /// <param name="book"></param>
+        /// <returns></returns>
+        Domain.DTOs.User AddUser(Domain.DTOs.User user);
+        /// <summary>
+        /// Method for delete user
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Domain.DTOs.User DeleteUser(int id);
+        /// <summary>
+        /// Get all users for response DTO
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<Domain.DTOs.User> GetAllUsersResponse();
+        /// <summary>
+        /// Get  user by id for response DTO
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Domain.DTOs.User GetUserByIdResponse(int id);
+        /// <summary>
+        /// Method for update user data
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="article"></param>
+        /// <returns></returns>
+        Domain.DTOs.User UpdateUser(int id, Domain.DTOs.User user);
     }
 }
