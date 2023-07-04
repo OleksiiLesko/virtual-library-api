bae98a9a2fcf05301105cd2cdac2c9210f4727bc
Oleksii Lesko <Oleksii.Lesko@nice.com>
2023-07-04 13:25:11 +0300

    Added JsoneIgnore for UserID

--- VirtualLibraryAPI.Domain/DTOs/User.cs
+++ VirtualLibraryAPI.Domain/DTOs/User.cs
@@ -2,11 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace VirtualLibraryAPI.Domain.DTOs
 {
-    internal class User
+    /// <summary>
+    /// User DTO
+    /// </summary>
+    public class User
     {
+        /// <summary>
+        ///  ID of user
+        /// </summary>
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public int? UserID { get; set; }
+        /// <summary>
+        /// First name of user
+        /// </summary>
+        public string FirstName { get; set; } 
+        /// <summary>
+        /// Last name of user
+        /// </summary>
+        public string LastName { get; set; } 
     }
 }
