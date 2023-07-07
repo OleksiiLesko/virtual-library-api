07c00da94848642cbf2dcc757392e6b7f3433322
Oleksii Lesko <Oleksii.Lesko@nice.com>
2023-07-07 17:11:56 +0300

    Added interface for validation user model

---
+++ VirtualLibraryAPI.Models/IValidationUserModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualLibraryAPI.Common;
+
+namespace VirtualLibraryAPI.Models
+{
+    /// <summary>
+    /// Validation user model interface 
+    /// </summary>
+    public interface IValidationUserModel
+    {
+        /// <summary>
+        ///  Check if user can reserve a copy
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        public ValidationUserStatus CanUserReserveCopy(int userId);
+    }
+}
