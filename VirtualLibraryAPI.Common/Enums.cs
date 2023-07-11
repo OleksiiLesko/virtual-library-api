8a9007345f838c1790a3c33441dad794280b9154
Oleksii Lesko <Oleksii.Lesko@nice.com>
2023-07-11 16:49:24 +0300

    Added UserType

--- VirtualLibraryAPI.Common/Enums.cs
+++ VirtualLibraryAPI.Common/Enums.cs
@@ -1,5 +1,26 @@
-﻿namespace VirtualLibraryAPI.Common
+﻿using System.ComponentModel;
+using System.Runtime.Serialization;
+
+namespace VirtualLibraryAPI.Common
 {
+    /// <summary>
+    /// Enumeration for user types
+    /// </summary>
+    public enum UserType
+    {
+        Administrator,
+        Client
+    }
+    /// <summary>
+    /// Type DTO
+    /// </summary>
+    public enum Type
+    {
+        Book = 1,
+        Article = 2,
+        Magazine = 3,
+        Copy = 4
+    }
     /// <summary>
     /// Enum status of copy vaidation
     /// </summary>
@@ -19,6 +40,7 @@
     {
         Valid,
         UserNotFound,
+        UserNotAdministrator,
         MaxCopiesExceeded,
         ExpiredCopy,
         DbError,
