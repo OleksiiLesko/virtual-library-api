bb179c35d86684883841462b579d9f134cca7966
Oleksii Lesko <Oleksii.Lesko@nice.com>
2023-07-13 15:11:01 +0300

    Added entity for UserType info table

---
+++ VirtualLibraryAPI.Domain/Entities/UserType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualLibraryAPI.Domain.Entities
+{
+    /// <summary>
+    /// Description for enum UserType
+    /// </summary>
+    public class UserType
+    {
+        /// <summary>
+        /// Type id
+        /// </summary>
+        public UserTypes UserTypeId { get; set; }
+        /// <summary>
+        /// Type name
+        /// </summary>
+        public string UserTypeName { get; set; } = string.Empty;
+        /// <summary>
+        /// Connect with User
+        /// </summary>
+        public virtual ICollection<User> User { get; set; }
+    }
+}
