a7bef621ea97ce1f8cc72051f647decc36ba2723
Oleksii Lesko <Oleksii.Lesko@nice.com>
2023-07-13 15:12:03 +0300

    Added enum for user(admin,client)

---
+++ VirtualLibraryAPI.Domain/Entities/UserTypes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualLibraryAPI.Domain.Entities
+{
+    public enum  UserTypes
+    {
+        Administrator = 0,
+        Client = 1
+    }
+}
