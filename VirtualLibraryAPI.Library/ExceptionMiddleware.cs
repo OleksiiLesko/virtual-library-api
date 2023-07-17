1cb4d57177d6b6ebb4de40cf9a8f17fa56e009da
Oleksii Lesko <Oleksii.Lesko@nice.com>
2023-07-17 16:37:56 +0300

    Changed name from Middleware to ExceptionMiddleware

---
+++ VirtualLibraryAPI.Library/ExceptionMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace VirtualLibraryAPI.Library
+{
+    public class ExceptionMiddleware
+    {
+        public RequestDelegate _requestDelegate;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+
+        public ExceptionMiddleware(RequestDelegate requestDelegate, ILogger<ExceptionMiddleware> logger)
+        {
+            _requestDelegate = requestDelegate;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _requestDelegate(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleException(context, ex);
+            }
+        }
+
+        private Task HandleException(HttpContext context, Exception ex)
+        {
+            _logger.LogError(ex.ToString());
+            var errorMessageObject = new { Message = ex.Message, Code = "system_error" };
+
+            var errorMessage = JsonConvert.SerializeObject(errorMessageObject);
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            return context.Response.WriteAsync(errorMessage);
+        }
+    }
+}
