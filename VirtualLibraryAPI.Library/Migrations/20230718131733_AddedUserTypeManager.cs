e1b8134180a948273eb264a702d120a998d06601
Oleksii Lesko <Oleksii.Lesko@nice.com>
2023-07-18 19:26:32 +0300

    Added user with type manager

---
+++ VirtualLibraryAPI.Library/Migrations/20230718131733_AddedUserTypeManager.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VirtualLibraryAPI.Library.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedUserTypeManager : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+            INSERT INTO Users (FirstName, LastName, UserTypes)
+            VALUES ('John', 'Doe', 2); ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
