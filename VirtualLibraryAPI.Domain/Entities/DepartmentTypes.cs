4ec3cae903ee6f9c0e10eba1b04e9b0fa8996746
Oleksii Lesko <Oleksii.Lesko@nice.com>
2023-07-20 13:32:29 +0300

    Added enum for department types

---
+++ VirtualLibraryAPI.Domain/Entities/DepartmentTypes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualLibraryAPI.Domain.Entities
+{
+    /// <summary>
+    /// Enumeration of department types
+    /// </summary>
+    public enum DepartmentTypes
+    {
+        Fantasy,
+        Adventure,
+        Science,
+        Romance,
+        Horror
+    }
+}
