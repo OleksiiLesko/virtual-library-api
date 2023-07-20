1e93c9ad05dc2dfcb5287b0c63c56a15c8e53c7a
Oleksii Lesko <Oleksii.Lesko@nice.com>
2023-07-20 13:33:05 +0300

    Added DepartmentTypeConfiguration

---
+++ VirtualLibraryAPI.Domain/EntitiesConfiguration/DepartmentTypeConfiguration.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualLibraryAPI.Common;
+using VirtualLibraryAPI.Domain.Entities;
+using DepartmentType = VirtualLibraryAPI.Domain.Entities.DepartmentType;
+
+namespace VirtualLibraryAPI.Domain.EntitiesConfiguration
+{
+    /// <summary>
+    /// Configuration for DepartmentType
+    /// </summary>
+    public class DepartmentTypeConfiguration : IEntityTypeConfiguration<DepartmentType>
+    {
+        /// <summary>
+        /// Builder configuration for UserType
+        /// </summary>
+        /// <param name="builder"></param>
+        public void Configure(EntityTypeBuilder<DepartmentType> builder)
+        {
+            builder.HasKey(e => e.TypeId);
+            builder.Property(e => e.TypeId)
+                .HasConversion<short>()
+                .IsRequired()
+                .ValueGeneratedNever();
+
+            builder.Property(e => e.TypeName)
+                .IsRequired()
+                .HasMaxLength(60)
+                .IsUnicode(false);
+
+            builder.HasData(Enum.GetValues(typeof(DepartmentTypes))
+                .Cast<DepartmentTypes>()
+                .Select(e => new DepartmentType
+                {
+                    TypeId = e,
+                    TypeName = e.ToString()
+                }));
+        }
+    }
+}
