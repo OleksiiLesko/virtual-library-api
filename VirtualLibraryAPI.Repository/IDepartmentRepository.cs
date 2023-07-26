ba7d3d8f05bb7d8234d1bf40dc622dc6f1ce49ed
Oleksii Lesko <Oleksii.Lesko@nice.com>
2023-07-26 16:26:55 +0300

    Added Department repository

---
+++ VirtualLibraryAPI.Repository/IDepartmentRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualLibraryAPI.Repository
+{
+    /// <summary>
+    /// Department repository interface 
+    /// </summary>
+    public interface IDepartmentRepository
+    {
+        /// <summary>
+        /// Method for get all departmens
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<Domain.DTOs.Department> GetAllDepartments();
+        /// <summary>
+        /// Method for get department by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Domain.DTOs.Department GetDepartmentById(int id);
+        /// <summary>
+        /// Method for add department
+        /// </summary>
+        /// <param name="department"></param>
+        /// <returns></returns>
+        Domain.DTOs.Department AddDepartment(Domain.DTOs.Department department);
+        /// <summary>
+        /// Method for update department
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="department"></param>
+        /// <returns></returns>
+        Domain.DTOs.Department UpdateDepartment(int id, Domain.DTOs.Department department);
+        /// <summary>
+        /// Method for delete department
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Domain.DTOs.Department DeleteDepartment(int id);
+        /// <summary>
+        /// Method for response get all departmens
+        /// </summary>
+        /// <returns></returns>
+         IEnumerable<Domain.DTOs.Department> GetAllDepartmentsResponse();
+    }
+}
