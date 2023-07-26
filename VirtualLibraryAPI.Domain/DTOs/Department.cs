76a6ff3ef4342ba890df222222fe443fbb237ba6
Oleksii Lesko <Oleksii.Lesko@nice.com>
2023-07-26 16:26:16 +0300

    Added Department DTO

---
+++ VirtualLibraryAPI.Domain/DTOs/Department.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace VirtualLibraryAPI.Domain.DTOs
+{
+    public class Department
+    {
+        /// <summary>
+        ///  ID of department
+        /// </summary>
+        [Required]
+        public int DepartmentID { get; set; }
+        /// <summary>
+        /// Name of department
+        /// </summary> 
+        [Required]
+        public string DepartmentName { get; set; } = string.Empty;
+    }
+}
