4e45600bf591ec453a2f8587a144143b1c483384
Oleksii Lesko <Oleksii.Lesko@nice.com>
2023-07-28 10:52:43 +0300

    Added Client entity

---
+++ VirtualLibraryAPI.Domain/Entities/Client.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualLibraryAPI.Domain.Entities
+{
+    /// <summary>
+    /// Table of clients
+    /// </summary>
+    public class Client
+    {
+        /// <summary>
+        ///  ID of client
+        /// </summary>
+        public int ClientID { get; set; }
+        /// <summary>
+        /// First name of user
+        /// </summary> 
+        public string FirstName { get; set; } = string.Empty;
+        /// <summary>
+        /// Last name of user
+        /// </summary>
+        public string LastName { get; set; } = string.Empty;
+        public virtual ICollection<Copy> Copies { get; set; }
+    }
+}
