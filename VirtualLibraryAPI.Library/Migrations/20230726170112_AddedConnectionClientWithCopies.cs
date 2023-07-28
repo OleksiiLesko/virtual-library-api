29059c64834cc30f4f2d2c595de7afee1e7f6582
Oleksii Lesko <Oleksii.Lesko@nice.com>
2023-07-28 10:55:56 +0300

    Added connection client table with copy

---
+++ VirtualLibraryAPI.Library/Migrations/20230726170112_AddedConnectionClientWithCopies.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VirtualLibraryAPI.Library.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedConnectionClientWithCopies : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Copies_Users_UserID",
+                table: "Copies");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Copies_UserID",
+                table: "Copies");
+
+            migrationBuilder.DropColumn(
+                name: "UserID",
+                table: "Copies");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserID",
+                table: "Copies",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Copies_UserID",
+                table: "Copies",
+                column: "UserID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Copies_Users_UserID",
+                table: "Copies",
+                column: "UserID",
+                principalTable: "Users",
+                principalColumn: "UserID");
+        }
+    }
+}
