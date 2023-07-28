11803884c1c27b6e33d5f195fcdd8c8ca01b2d34
Oleksii Lesko <Oleksii.Lesko@nice.com>
2023-07-28 10:52:09 +0300

    Added Client DTO

---
+++ VirtualLibraryAPI.Domain/DTOs/Client.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace VirtualLibraryAPI.Domain.DTOs
+{
+    /// <summary>
+    /// Client DTO
+    /// </summary>
+    public class Client
+    {
+        /// <summary>
+        ///  ID of user
+        /// </summary>
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public int? ClientID { get; set; }
+        /// <summary>
+        /// First name of user
+        /// </summary>
+        [Required]
+        public string FirstName { get; set; }
+        /// <summary>
+        /// Last name of user
+        /// </summary>
+        [Required]
+        public string LastName { get; set; }
+    }
+}
