d49737094ba6183e87b35e979944b402480ab707
Oleksii Lesko <Oleksii.Lesko@nice.com>
2023-07-28 10:58:03 +0300

    Added interface for validation client

---
+++ VirtualLibraryAPI.Models/IValidationClientModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualLibraryAPI.Common;
+
+namespace VirtualLibraryAPI.Models
+{
+    /// <summary>
+    /// Validation client model interface 
+    /// </summary>
+    public interface IValidationClientModel
+    {
+        /// <summary>
+        ///  Check if client can reserve a copy
+        /// </summary>
+        /// <param name="clientId"></param>
+        /// <returns></returns>
+        public ValidationUserStatus CanClientReserveCopy(int clientId);
+    }
+}
