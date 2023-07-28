c1c0a772de0c199a24340b84cff9f2f5f1cd4ac4
Oleksii Lesko <Oleksii.Lesko@nice.com>
2023-07-28 11:01:38 +0300

    Added interface IClientRepository

---
+++ VirtualLibraryAPI.Repository/IClientRepository.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualLibraryAPI.Repository
+{
+    /// <summary>
+    /// Client repository interface 
+    /// </summary>
+    public interface IClientRepository
+    {
+        /// <summary>
+        /// Method for get all clients
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<Domain.DTOs.Client> GetAllClients();
+        /// <summary>
+        /// Method for get client by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Domain.DTOs.Client GetClientById(int id);
+        /// <summary>
+        /// Method for add client
+        /// </summary>
+        /// <param name="book"></param>
+        /// <returns></returns>
+        Domain.DTOs.Client AddClient(Domain.DTOs.Client client);
+        /// <summary>
+        /// Method for delete client
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Domain.DTOs.Client DeleteClient(int id);
+        /// <summary>
+        /// Get all clients for response DTO
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<Domain.DTOs.Client> GetAllClientsResponse();
+        /// <summary>
+        /// Get  client by id for response DTO
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Domain.DTOs.Client GetClientByIdResponse(int id);
+        /// <summary>
+        /// Method for update client data
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="article"></param>
+        /// <returns></returns>
+        Domain.DTOs.Client UpdateClient(int id, Domain.DTOs.Client client);
+        /// <summary>
+        /// Count client copies
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        public int CountClientCopies(int userId);
+        /// <summary>
+        /// Check if user have expired copies
+        /// </summary>
+        /// <param name="clientId"></param>
+        /// <returns></returns>
+        public bool HasExpiredCopy(int clientId);
+    }
+}
