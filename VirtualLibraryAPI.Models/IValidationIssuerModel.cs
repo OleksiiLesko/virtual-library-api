3d3653966bfe0d7542b8cf24ee7412d3c3e46741
Oleksii Lesko <Oleksii.Lesko@nice.com>
2023-07-28 10:59:12 +0300

    Added interface IValidationIssuerModel for validate if issuer can issue a copy

---
+++ VirtualLibraryAPI.Models/IValidationIssuerModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualLibraryAPI.Common;
+
+namespace VirtualLibraryAPI.Models
+{
+    /// <summary>
+    /// Validation issuer model interface 
+    /// </summary>
+    public interface IValidationIssuerModel
+    {
+        /// <summary>
+        ///  Check if client can reserve a copy
+        /// </summary>
+        /// <param name="clientId"></param>
+        /// <returns></returns>
+        public ValidationIssuerStatus CanIssuerIssueCopy(int useriId, int copyId);
+    }
+}
